bbd56a52838f8600255d2055cb0fa2034acba6c1
BeomyeonAndrewKim <beom911@gmail.com>
2019-07-08 17:27:33 +0900

    test: add HookCounter test case

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Profile from "./Profile";
 import Counter from "./Counter";
+import HookCounter from "./HookCounter";
 
 function App() {
   return (
     <div className="App">
       <Profile username="Andrea" name="beomyeon" />
       <Counter />
+      <HookCounter />
     </div>
   );
 }
