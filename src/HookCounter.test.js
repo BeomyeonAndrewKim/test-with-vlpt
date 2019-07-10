5224925bf3bd526dc5013bfbba7c0cc46ea5ad70
BeomyeonAndrewKim <beom911@gmail.com>
2019-07-10 14:20:48 +0900

    test: add HookCounter test case with testing-library/react

--- src/HookCounter.test.js
+++ src/HookCounter.test.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { mount } from "enzyme";
+import { render, fireEvent } from "@testing-library/react";
 import HookCounter from "./HookCounter";
 
-describe("<HookCounter/>", () => {
+describe("<HookCounter/> with enzyme", () => {
   it("matches snapshot", () => {
     const wrapper = mount(<HookCounter />);
     expect(wrapper).toMatchSnapshot();
@@ -33,5 +34,38 @@ describe("<HookCounter/>", () => {
     const number = wrapper.find("h2");
 
     expect(number.text()).toBe("-2");
+  });
+});
+
+describe("<HookCounter/> with react-testing-library", () => {
+  const utils = render(<HookCounter />);
+  it("matches snapshot", () => {
+    expect(utils.container).toMatchSnapshot();
+  });
+
+  it("has a number and two buttons", () => {
+    utils.getByText("0");
+    utils.getByText("+1");
+    utils.getByText("-1");
+  });
+
+  it("increase", () => {
+    const number = utils.getByText("0"),
+      plusButton = utils.getByText("+1");
+
+    fireEvent.click(plusButton);
+    fireEvent.click(plusButton);
+
+    expect(number).toHaveTextContent("2"); // jest-dom의 확장 matcher 사용
+    expect(number.textContent).toBe("2"); // textContent를 직접 비교
+  });
+
+  it("decrease", () => {
+    const number = utils.getByText("2"),
+      minusButton = utils.getByText("-1");
+
+    fireEvent.click(minusButton);
+    fireEvent.click(minusButton);
+    expect(number).toHaveTextContent("0"); // jest-dom의 확장 matcher 사용
   });
 });
