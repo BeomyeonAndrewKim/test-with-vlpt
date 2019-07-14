ac9a9728045375cfa0e6b139f33bebc81f5ae3c6
BeomyeonAndrewKim <beom911@gmail.com>
2019-07-14 23:56:38 +0900

    test: add TodoForm onChange test case

--- src/TodoForm.test.js
+++ src/TodoForm.test.js
@@ -3,7 +3,10 @@ import { render, fireEvent } from "@testing-library/react";
 import TodoForm from "./TodoForm";
 
 describe("<TodoForm/>", () => {
-  const { getByText, getByPlaceholderText } = render(<TodoForm />);
+  const onInsert = jest.fn();
+  const { getByText, getByPlaceholderText } = render(
+    <TodoForm onInsert={onInsert} />
+  );
   it("has input and button", () => {
     getByPlaceholderText("할 일을 입력하세요.");
     getByText("등록");
@@ -17,5 +20,20 @@ describe("<TodoForm/>", () => {
       }
     });
     expect(input).toHaveAttribute("value", "TDD 배우기");
+  });
+
+  it("calls onInsert and clear input", () => {
+    const input = getByPlaceholderText("할 일을 입력하세요.");
+    const button = getByText("등록");
+
+    fireEvent.change("input", {
+      target: {
+        value: "TDD 배우기"
+      }
+    });
+
+    fireEvent.click(button);
+    expect(onInsert).toBeCalledWith("TDD 배우기");
+    expect(input).toHaveAttribute("value", "");
   });
 });
